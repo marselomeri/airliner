87230dcf7eaed98d627cb0bf8277b994d9d097f7
Mathew Benson <mbenson@windhoverlabs.com>
2017-09-01 02:06:16 +0000

    Fixed failing osal test.

--- config/softsim/default/set-vars.cmake
+++ config/softsim/default/set-vars.cmake
@@ -1,5 +1,5 @@
-set(PSP ${PROJECT_SOURCE_DIR}/psp/fsw/pc-linux-nasa)
-set(OSAL ${PROJECT_SOURCE_DIR}/osal/src/os/posix-nasa)
+set(PSP ${PROJECT_SOURCE_DIR}/psp/fsw/pc-linux)
+set(OSAL ${PROJECT_SOURCE_DIR}/osal/src/os/posix)
 
 
 
