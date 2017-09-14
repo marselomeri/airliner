6911c58c113027341896d600580641951e15985a
ldoman <ldoman@windhoverlabs.com>
2017-09-14 16:28:53 +0000

    Undefining serialization macros

--- config/softsim/default/inc/ci_platform_cfg.h
+++ config/softsim/default/inc/ci_platform_cfg.h
@@ -128,8 +128,8 @@ extern "C" {
 /* TODO:  Add Doxygen markup. */
 #define CI_CUSTOM_DEV_PATH       "/dev/ttyUSB0"
 
-#define CI_SERIALIZED	(1)
-#define CI_DEBUG_SERIALIZED	(1)
+//#define CI_SERIALIZED	(1)
+//#define CI_DEBUG_SERIALIZED	(1)
 #define CI_MAX_ENC_LEN (32768)
 
 
