8b941b71214c478e2d5ae0d604de343ff7c067f8
Mathew Benson <mbenson@windhoverlabs.com>
2017-09-29 18:52:57 +0000

    Fixed typos in the license boiler plate.

--- apps/pblib/fsw/public_inc/pb_lib.h
+++ apps/pblib/fsw/public_inc/pb_lib.h
@@ -1,6 +1,6 @@
 /****************************************************************************
  *
- *   Copyright (c) 2016 Windhover Labs, L.L.C. All rights reserved.
+ *   Copyright (c) 2016-2017 Windhover Labs, L.L.C. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -12,8 +12,8 @@
  *    notice, this list of conditions and the following disclaimer in
  *    the documentation and/or other materials provided with the
  *    distribution.
- * 3. Neither the name PX4 nor the names of its contributors may be
- *    used to endorse or promote products derived from this software
+ * 3. Neither the name Windhover Labs nor the names of its contributors 
+ *    may be used to endorse or promote products derived from this software
  *    without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
