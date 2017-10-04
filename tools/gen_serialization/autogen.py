d3c28a48616f5ff449f6850e4a7fc168dea9ae1b
ldoman <ldoman@windhoverlabs.com>
2017-10-04 13:18:09 -0500

    Autogenerator now also generates python pb modules

--- tools/gen_serialization/autogen.py
+++ tools/gen_serialization/autogen.py
@@ -7,6 +7,7 @@ from jinja2 import Environment, FileSystemLoader
 import os
 from os.path import join
 from shutil import copyfile, rmtree
+import subprocess
 
 extras = {"autogen_version": "0.0.0.1"}
 
@@ -38,8 +39,11 @@ with open(join(temp, "cookiecutter.json"), 'r') as f:
 # Copy proto template file for every proto msg
 for proto_msg, proto_data in cookie_json["proto_msgs"].iteritems():
 	pb_template = join(temp, "template.proto")
+	pb_py_template = join(temp, "py_template.proto")
 	out_template = join(temp, "{{cookiecutter.dir_name}}", proto_data["proto_msg"][:-3] + ".proto")
+	py_out_template = join(temp, "{{cookiecutter.dir_name}}", "_py_" + proto_data["proto_msg"][:-3] + ".proto")
 	copyfile(pb_template, out_template)
+	copyfile(pb_py_template, py_out_template)
 
 cookiecutter(temp, no_input=True, extra_context=extras, overwrite_if_exists=True)
 
@@ -55,8 +59,21 @@ for dir in os.listdir(target):
 	for f in os.listdir(dir):
 		src = join(target, dir, f)
 		dest = join(target, f)
+		if f[:4] == "_py_":
+			subprocess.call(['protoc', "-I", join(target, dir), '--python_out=' + target, src])
 		copyfile(src, dest)
 	rmtree(join(target, dir))
+
+# Organize python files
+py_out = join(target, "python_pb")
+os.mkdir(py_out)
+for f in os.listdir(target):
+	src = join(target, f)
+	dest = join(py_out, f[4:])
+	if f[:4] == "_py_":
+		if f[-2:] == "py":
+			copyfile(src, dest)
+		os.remove(src)
 
 rmtree(temp)
 
