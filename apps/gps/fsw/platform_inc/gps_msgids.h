4b5df5d8170aaeb3bdeaeccefd2d431fd61eadef
ldoman <ldoman@windhoverlabs.com>
2017-11-08 21:04:51 +0000

    Adding gps app code

---
+++ apps/gps/fsw/platform_inc/gps_msgids.h
@@ -0,0 +1,21 @@
+#ifndef GPS_MSGIDS_H
+#define GPS_MSGIDS_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define GPS_HK_TLM_MID    0x0000
+#define GPS_SEND_HK_MID   0x0000
+#define GPS_READ_SENSOR_MID       0x0000
+#define GPS_CMD_MID       0x0000
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* GPS_MSGIDS_H */
+
+/************************/
+/*  End of File Comment */
+/************************/
