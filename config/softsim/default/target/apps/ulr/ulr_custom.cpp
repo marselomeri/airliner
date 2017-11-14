03b34f23cd5bec20258d621f6e5d502d69059aea
Mathew Benson <mbenson@windhoverlabs.com>
2017-11-14 17:20:22 +0000

    Merged in sprint-8 commits and resolved conflicts.

--- config/softsim/default/target/apps/ulr/ulr_custom.cpp
+++ config/softsim/default/target/apps/ulr/ulr_custom.cpp
@@ -1,16 +1,17 @@
 #include "ulr_app.h"
+#include <unistd.h>
 
 int32 ULR::InitDevice(void)
 {
-	OS_printf("ULR::InitDevice\n");
-
 	return 0;
 }
 
 
-int32 ULR::ReadDevice(void)
+int32 ULR::ReadDevice(uint8 *Buffer, uint32 *Size)
 {
-	OS_printf("ULR::ReadDevice\n");
+	*Size = 0;
+
+	sleep(1);	
 
 	return 0;
 }
