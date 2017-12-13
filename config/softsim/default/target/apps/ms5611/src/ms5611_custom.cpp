f9012162ad55d834b803895a248d2c471271d9f3
Mathew Benson <mbenson@windhoverlabs.com>
2017-12-13 00:45:14 +0000

    Integrated ms5611 with Gazebo sim.

--- config/softsim/default/target/apps/ms5611/src/ms5611_custom.cpp
+++ config/softsim/default/target/apps/ms5611/src/ms5611_custom.cpp
@@ -38,33 +38,46 @@
 ** Includes
 *************************************************************************/
 #include "cfe.h"
-#include "ms5611_spi.h"
 #include "ms5611_events.h"
 #include "ms5611_perfids.h"
+#include "ms5611_app.h"
 
 #include <unistd.h>
-#include <fcntl.h>
-#include <sys/ioctl.h>
-#include <linux/types.h>
-#include <linux/spi/spidev.h>
-#include <sys/wait.h>
 #include <errno.h>
 #include <string.h>
+#include "simlib.h"
+#include <math.h>
+#include "ms5611_custom.h"
 
 /************************************************************************
 ** Local Defines
 *************************************************************************/
+
+#define MS5611_CUSTOM_C1	(40127)
+#define MS5611_CUSTOM_C2	(36924)
+#define MS5611_CUSTOM_C3	(23317)
+#define MS5611_CUSTOM_C4	(23282)
+#define MS5611_CUSTOM_C5	(33464)
+#define MS5611_CUSTOM_C6	(28312)
+
 
 /************************************************************************
 ** Local Structure Declarations
 *************************************************************************/
 
 typedef enum {
-	MS5611_CUSTOM_UNINITIALIZED = 0;
-	MS5611_CUSTOM_D1_CONV       = 1;
-	MS5611_CUSTOM_D2_CONV       = 2;
+	MS5611_CUSTOM_UNINITIALIZED = 0,
+	MS5611_CUSTOM_D1_CONV       = 1,
+	MS5611_CUSTOM_D2_CONV       = 2
 } MS5611_Custom_State_t;
 
+
+typedef struct
+{
+	MS5611_Custom_State_t State;
+	uint32 D1;
+	uint32 D2;
+} MS5611_AppCustomData_t;
 
 
 typedef enum {
@@ -106,12 +119,12 @@ typedef enum {
 /************************************************************************
 ** External Global Variables
 *************************************************************************/
+extern MS5611 oMS5611;
 
 /************************************************************************
 ** Global Variables
 *************************************************************************/
 MS5611_AppCustomData_t MS5611_AppCustomData;
-struct spi_ioc_transfer MS5611_SPI_Xfer[2];
 
 /************************************************************************
 ** Local Variables
@@ -142,7 +155,49 @@ boolean MS5611_Custom_Uninit(void)
 
 boolean MS5611_ReadPROM(uint8 Addr, uint16 *returnVal)
 {
-	*returnVal = 1;
+	switch(Addr)
+	{
+		case 0:
+			*returnVal = 0;
+			break;
+
+		case 1:
+			*returnVal = MS5611_CUSTOM_C1;
+			break;
+
+		case 2:
+			*returnVal = MS5611_CUSTOM_C2;
+			break;
+
+		case 3:
+			*returnVal = MS5611_CUSTOM_C3;
+			break;
+
+		case 4:
+			*returnVal = MS5611_CUSTOM_C4;
+			break;
+
+		case 5:
+			*returnVal = MS5611_CUSTOM_C5;
+			break;
+
+		case 6:
+			*returnVal = MS5611_CUSTOM_C6;
+			break;
+
+		case 7:
+		{
+		    *returnVal = 0x0F & oMS5611.CRC4(oMS5611.MS5611_Coefficients);
+
+			break;
+		}
+
+		default:
+		{
+			*returnVal = 0;
+			break;
+		}
+	}
 
     return TRUE;
 }
@@ -150,19 +205,102 @@ boolean MS5611_ReadPROM(uint8 Addr, uint16 *returnVal)
 
 boolean MS5611_D1Conversion(void)
 {
+	MS5611_AppCustomData.State = MS5611_CUSTOM_D1_CONV;
+
     return TRUE;
 }
 
 
 boolean MS5611_D2Conversion(void)
 {
+	MS5611_AppCustomData.State = MS5611_CUSTOM_D2_CONV;
+
     return TRUE;
 }
 
 
 boolean MS5611_ReadADCResult(uint32 *returnVal)
 {
-    return TRUE;
+	boolean result = FALSE;
+
+	if(returnVal == 0)
+	{
+		result = FALSE;
+	}
+	else
+	{
+		switch(MS5611_AppCustomData.State)
+		{
+			case MS5611_CUSTOM_D1_CONV:
+			{
+				float Altitude = 0.0f;
+				float Temperature = 0.0f;
+		        /* tropospheric properties (0-11km) for standard atmosphere */
+		        /* temperature at base height in Kelvin */
+		        const double T1 = 15.0 + 273.15;
+		        /* temperature gradient in degrees per metre */
+		        const double a  = -6.5 / 1000;
+		        /* gravity constant in m/s/s */
+		        const double g  = 9.80665;
+		        /* ideal gas constant in J/kg/K */
+		        const double R  = 287.05;
+
+		        /* current pressure at MSL in kPa */
+		        double p1 = 101325 / 1000.0;
+
+		        /* measured pressure in kPa */
+		        double p = 0.0f;
+
+		        /* Difference between actual and reference temperature. */
+		        int32 dT = 0;
+
+		        /* Offset at actual temperature */
+		        int64 OFF = 0;
+
+		        /* Sensitivity at actual temperature */
+		        int64 SENS = 0;
+
+				SIMLIB_GetPressureAltitude(&Altitude);
+				SIMLIB_GetTemp(&Temperature);
+
+		        p = p1*pow(((a*(Altitude)+T1)/T1),(-g/(a*R)));
+		        dT = (0x800000*((Temperature*100)-2000)) / MS5611_CUSTOM_C5;
+
+		        MS5611_AppCustomData.D2 = 0x100*MS5611_CUSTOM_C5+dT;
+
+		        OFF = MS5611_CUSTOM_C2 * 0x10000ll  + (dT * MS5611_CUSTOM_C4) / (0x80ll);
+
+		        SENS = (MS5611_CUSTOM_C1*0x8000ll)+((MS5611_CUSTOM_C3*dT)/0x0100ll);
+
+		        MS5611_AppCustomData.D1 = (0x200000*(OFF+0x8000*(p*1000)))/SENS;
+
+				*returnVal = MS5611_AppCustomData.D1;
+				result = TRUE;
+
+				break;
+			}
+
+			case MS5611_CUSTOM_D2_CONV:
+			{
+				*returnVal = MS5611_AppCustomData.D2;
+
+				result = TRUE;
+
+				break;
+			}
+
+			default:
+			{
+				*returnVal = 0;
+
+				result = FALSE;
+
+				break;
+			}
+		}
+	}
+
+    return result;
 }
 
 
