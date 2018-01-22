0c81c33b3647def92609c60599a6c064663d5f86
cnalley <cnalley@windhoverlabs.com>
2018-01-22 19:42:52 +0000

    Temp change: hard coding D2 temp value for baro.

--- config/typhoon_h480/sitl/target/apps/ms5611/src/ms5611_custom.cpp
+++ config/typhoon_h480/sitl/target/apps/ms5611/src/ms5611_custom.cpp
@@ -267,21 +267,25 @@ boolean MS5611_ReadADCResult(uint32 *returnVal)
                 {
                     Temperature = 32;
                 }
+                Temperature = 32;
 
                 /* Alternate form 
                  * p = p1*pow((a*((T1/a)+Altitude))/T1,(-g/(a*R)));
                  */
-		        p = 1000 * (p1*pow(((a*(Altitude)+T1)/T1),(-g/(a*R))));
+		        p = p1*pow(((a*(Altitude)+T1)/T1),(-g/(a*R)));
                 /* Check */
-		        dT = ((((Temperature*100)-2000)) * 0x800000) / MS5611_CUSTOM_C6;
+		        dT = (0x800000*((Temperature*100)-2000)) / MS5611_CUSTOM_C6;
+
                 /* Check */
-		        MS5611_AppCustomData.D2 = dT + (MS5611_CUSTOM_C5 >> 8);
+		        //MS5611_AppCustomData.D2 = dT + (MS5611_CUSTOM_C5 >> 8);
+                MS5611_AppCustomData.D2 = 8566784;
+
                 /* */
 		        OFF = ((int64)MS5611_CUSTOM_C2 << 16)  + (((int64)MS5611_CUSTOM_C4 * dT) >> 7);
 
 		        SENS = ((int64)MS5611_CUSTOM_C1 << 15) + (((int64)dT * MS5611_CUSTOM_C3) >> 8);
 
-		        MS5611_AppCustomData.D1 = (OFF + (p * 0x8000) * 0x200000) / SENS;
+		        MS5611_AppCustomData.D1 = (0x200000*(OFF+0x8000*(p*1000)))/SENS;
 
 				*returnVal = MS5611_AppCustomData.D1;
 				result = TRUE;
