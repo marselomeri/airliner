6c49de116898c83e0b17fbb2e067bfff3aa8b63b
ldoman <ldoman@windhoverlabs.com>
2018-02-20 19:10:28 +0000

    More math fixes

--- apps/px4lib/fsw/src/math/Matrix10F3.cpp
+++ apps/px4lib/fsw/src/math/Matrix10F3.cpp
@@ -187,7 +187,7 @@ Vector10F Matrix10F3::operator*(const Vector3F &vecIn)
     {
     	for(int j = 0; j < COLS; j++)
 		{
-    		value += data[i][j]*vecIn[j];
+    		vecOut[i] += data[i][j]*vecIn[j];
 		}
     }
 
