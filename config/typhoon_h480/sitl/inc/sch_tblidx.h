f51942ae09091455a5e94a8cf8247c7f52793527
cnalley <cnalley@windhoverlabs.com>
2018-03-31 23:21:45 +0000

    Adding missing LGC custom layer in SITL and added LGC to sch table.

--- config/typhoon_h480/sitl/inc/sch_tblidx.h
+++ config/typhoon_h480/sitl/inc/sch_tblidx.h
@@ -87,6 +87,9 @@
 #define SENS_SEND_HK_MIDX			90
 #define SENS_WAKEUP_MIDX			91
 
+#define LGC_WAKEUP_MIDX             93
+#define LGC_SEND_HK_MIDX 			94
+
 #define PX4BR_PROC_CMD_MIDX			100
 #define PX4BR_SEND_HK_MIDX			101
 #define AE_SEND_HK_MIDX			    102
