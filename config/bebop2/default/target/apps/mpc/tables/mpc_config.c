36c2a41a9ca2902d8e85b7eb432ca918fd9adcae
Luke Doman <ldoman@windhoverlabs.com>
2018-08-13 08:47:05 -0500

    Updating vehicle mpc tables to tuned pids

--- config/bebop2/default/target/apps/mpc/tables/mpc_config.c
+++ config/bebop2/default/target/apps/mpc/tables/mpc_config.c
@@ -148,7 +148,7 @@ MPC_ConfigTbl_t MPC_ConfigTbl =
 	 * @decimal 2
 	 * @group Multicopter Position Control
 	 */
-	0.2f,
+	0.15f,
 
 	/**
 	 * MPC_Z_VEL_I
@@ -234,7 +234,7 @@ MPC_ConfigTbl_t MPC_ConfigTbl =
 	 * @decimal 2
 	 * @group Multicopter Position Control
 	 */
-	0.09,
+	0.05,
 
 	/**
 	 * MPC_XY_VEL_I
@@ -247,7 +247,7 @@ MPC_ConfigTbl_t MPC_ConfigTbl =
 	 * @decimal 3
 	 * @group Multicopter Position Control
 	 */
-	0.02f,
+	0.03f,
 
 	/**
 	 * MPC_XY_VEL_D
@@ -258,7 +258,7 @@ MPC_ConfigTbl_t MPC_ConfigTbl =
 	 * @decimal 3
 	 * @group Multicopter Position Control
 	 */
-	0.01f,
+	0.005f,
 
 	/**
 	 * MPC_XY_CRUISE
