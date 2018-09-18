8bfb0648a4e3cbc292d6770a31b4a0b13a4f1f43
cnalley <cnalley@windhoverlabs.com>
2018-09-18 13:29:44 +0000

    Implements WINDEV-705 convert from fifo to lifo in custom mpu6050.

--- config/bebop2/default/target/apps/mpu6050/src/mpu6050_custom.c
+++ config/bebop2/default/target/apps/mpu6050/src/mpu6050_custom.c
@@ -569,7 +569,8 @@ boolean MPU6050_Measure(MPU6050_SampleQueue_t *SampleQueue)
     SampleQueue->SampleCount = fifoByteCount / sizeof(MPU6050_Sample_t);
     SampleQueue->SampleIntervalUs = 1000 / MPU6050_AppCustomData.FifoSamplesPerCycle;
 
-    for(index = 0; index < SampleQueue->SampleCount; ++index)
+    /* Convert from FIFO to LIFO. */
+    for(index = SampleQueue->SampleCount - 1; index >= 0; --index)
     {
         //returnBool = MPU6050_sampleChecks(&sample[index]);
         //if(FALSE == returnBool)
