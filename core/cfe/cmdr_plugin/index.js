8b360d4ec1fffe441c98f5d32be2f644895d3566
Mathew Benson <mbenson@windhoverlabs.com>
2018-09-26 04:28:38 +0000

    Updated the CFE commander plugin

--- core/cfe/cmdr_plugin/index.js
+++ core/cfe/cmdr_plugin/index.js
@@ -2,7 +2,7 @@
 
 var path = require('path');
 
-const CdrPlugin = require('/home/users/mbenson/git/airliner/tools/commander/commander/classes/CdrPlugin');
+const CdrPlugin = require(path.join(global.CDR_INSTALL_DIR, '/commander/classes/CdrPlugin'));
 //var express = require('express');
 //var router = express.Router();
 //var path = require('path');
@@ -40,70 +40,97 @@ module.exports = class CfeCdrPlugin extends CdrPlugin {
 	getPanels() {
 		var result = {
 			text: 'Core Flight Executive',
+			description: 'Core Flight Executive services.',
 			nodes: [
 				{
 					text: 'Essential Services',
+					description: 'Core essential services.',
 					nodes: [
-						{
-							text: 'Main',
-							filePath: '/es/main.pug',
-							urlPath: '/cfe/es/main.pug',
-				        },
 						{
 							text: 'Application Control',
 							filePath: '/es/appctrl.pug',
 							urlPath: '/cfe/es/appctrl.pug',
+							description: 'Application start, stop, restart, and reload.'
 				        },
 						{
-							text: 'Console',
-							filePath: '/es/console.pug',
-							urlPath: '/cfe/es/console.pug',
+							text: 'Command and Data Handling',
+							filePath: '/es/cdh.pug',
+							urlPath: '/cfe/es/cdh.pug',
+							description: 'Command counters.'
+				        },
+                        {
+                            text: 'Console',
+                            filePath: '/es/console.pug',
+                            urlPath: '/cfe/es/console.pug',
+                            description: 'System console.'
+                        },
+						{
+							text: 'Logging',
+							filePath: '/es/logging.pug',
+							urlPath: '/cfe/es/logging.pug',
+							description: 'Essential Services logging status and control.'
+				        },
+                        {
+                            text: 'Memory',
+                            filePath: '/es/memory.pug',
+                            urlPath: '/cfe/es/memory.pug',
+                            description: 'Memory and memory pool status and monitoring.'
+                        },
+						{
+							text: 'Reset',
+							filePath: '/es/reset.pug',
+							urlPath: '/cfe/es/reset.pug',
+							description: 'Core reset and restart.'
 				        },
 						{
-							text: 'Memory',
-							filePath: '/es/memory.pug',
-							urlPath: '/cfe/es/memory.pug',
-				        },
-						{
-							text: 'Performance Monitor',
-							filePath: '/es/perfmon.pug',
-							urlPath: '/cfe/es/perfmon.pug',
+							text: 'Version',
+							filePath: '/es/version.pug',
+							urlPath: '/cfe/es/version.pug',
+							description: 'Core versions.'
 				        }
 			        ]
 			    },{
+                    text: 'Event Services',
+                    description: 'Core event services.',
+                    nodes: [
+                        {
+                            text: 'Main',
+                            filePath: '/evs/main.pug',
+                            urlPath: '/cfe/evs/main.pug',
+                            description: ''
+                        }
+                    ]
+                },{
 					text: 'Software Bus',
+                    description: 'Core software bus services.',
 					nodes: [
 						{
 							text: 'Main',
 							filePath: '/sb/main.pug',
 							urlPath: '/cfe/sb/main.pug',
+                            description: ''
 				        }
 					]
 			    },{
 					text: 'Table Services',
+                    description: 'Core table services.',
 					nodes: [
 						{
 							text: 'Main',
 							filePath: '/tbl/main.pug',
 							urlPath: '/cfe/tbl/main.pug',
+                            description: ''
 				        }
 					]
 			    },{
 					text: 'Time Services',
+                    description: 'Core time services.',
 					nodes: [
 						{
 							text: 'Main',
 							filePath: '/time/main.pug',
 							urlPath: '/cfe/time/main.pug',
-				        }
-					]
-			    },{
-					text: 'Event Services',
-					nodes: [
-						{
-							text: 'Main',
-							filePath: '/evs/main.pug',
-							urlPath: '/cfe/evs/main.pug',
+                            description: ''
 				        }
 					]
 			    }
