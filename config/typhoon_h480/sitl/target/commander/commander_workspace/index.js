aa0c2bcf24877500a0c213130bba163ed6ee1c91
Mathew Benson <mbenson@windhoverlabs.com>
2018-10-24 17:44:48 -0500

    Added initial code for the commander config database plugin.

--- config/typhoon_h480/sitl/target/commander/commander_workspace/index.js
+++ config/typhoon_h480/sitl/target/commander/commander_workspace/index.js
@@ -6,8 +6,9 @@ var BinaryEncoder = require(path.join(global.CDR_INSTALL_DIR, 'binary-encoder'))
 var BinaryDecoder = require(path.join(global.CDR_INSTALL_DIR, 'binary-decoder'));
 var UdpStdProvider = require(path.join(global.CDR_INSTALL_DIR, 'udp-std-provider'));
 var VariableServer = require(path.join(global.CDR_INSTALL_DIR, 'variable-server'));
-//var ProtobufEncoder = require(path.join(global.CDR_INSTALL_DIR, 'protobuf-encoder'));
-//var ProtobufDecoder = require(path.join(global.CDR_INSTALL_DIR, 'protobuf-decoder'));
+var ProtobufEncoder = require(path.join(global.CDR_INSTALL_DIR, 'protobuf-encoder'));
+var ProtobufDecoder = require(path.join(global.CDR_INSTALL_DIR, 'protobuf-decoder'));
+var ConfigDatabase = require(path.join(global.CDR_INSTALL_DIR, 'config-database'));
 
 var commander = new Commander(global.CDR_WORKSPACE, `${global.CDR_WORKSPACE}/etc/commander-config.json`);
 var binaryEncoder = new BinaryEncoder(global.CDR_WORKSPACE, `${global.CDR_WORKSPACE}/etc/binary-encoder-config.json`);
@@ -15,8 +16,9 @@ var binaryDecoder = new BinaryDecoder(global.CDR_WORKSPACE, `${global.CDR_WORKSP
 var variableServer = new VariableServer(`${global.CDR_WORKSPACE}/etc/variable-server-config.json`);
 var fswConnector = new UdpStdProvider(`${global.CDR_WORKSPACE}/etc/udpstdprovider-config.json`);
 var pylinerConnector = new UdpStdProvider(`${global.CDR_WORKSPACE}/etc/pyliner-connector-config.json`);
-//var protobufEncoder = new ProtobufEncoder(global.CDR_WORKSPACE, `${global.CDR_WORKSPACE}/etc/protobuf-encoder-config.json`);
-//var protobufDecoder = new ProtobufDecoder(global.CDR_WORKSPACE, `${global.CDR_WORKSPACE}/etc/protobuf-decoder-config.json`);
+var protobufEncoder = new ProtobufEncoder(global.CDR_WORKSPACE, `${global.CDR_WORKSPACE}/etc/protobuf-encoder-config.json`);
+var protobufDecoder = new ProtobufDecoder(global.CDR_WORKSPACE, `${global.CDR_WORKSPACE}/etc/protobuf-decoder-config.json`);
+var configDB = new ConfigDatabase(global.CDR_WORKSPACE, `${global.CDR_WORKSPACE}/etc/config-database-config.json`);
 
 var airliner = commander.addInstance('airliner', function(instance) {
 	instance.addApp('binary-encoder',    binaryEncoder);
@@ -24,8 +26,9 @@ var airliner = commander.addInstance('airliner', function(instance) {
 	instance.addApp('fsw-connector',     fswConnector);
 	instance.addApp('pyliner-connector', pylinerConnector);
 	instance.addApp('variable-server',   variableServer);
-	//instance.addApp('protobuf-encoder',  protobufEncoder);
-	//instance.addApp('protobuf-decoder',  protobufDecoder);
+	instance.addApp('protobuf-encoder',  protobufEncoder);
+	instance.addApp('protobuf-decoder',  protobufDecoder);
+	instance.addApp('config-database',   configDB);
 	
 	commander.setDefaultInstance(instance);
 	
