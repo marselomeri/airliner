0b273bca5b812ee8bc1e9bb6e09672c88f1c51da
Mathew Benson <mbenson@windhoverlabs.com>
2019-03-06 22:10:52 -0600

    Squashed 'apps/sonar/' content from commit 89f2cef
    git-subtree-dir: apps/sonar
    git-subtree-split: 89f2ceff1fabb96b9ebf32922632f1cb70ce378f

---
+++ fsw/platform_inc/sonar_msgids.h
@@ -0,0 +1,21 @@
+#ifndef SONAR_MSGIDS_H
+#define SONAR_MSGIDS_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define SONAR_HK_TLM_MID    0x0000
+#define SONAR_SEND_HK_MID   0x0000
+#define SONAR_MEASURE_MID   0x0000
+#define SONAR_CMD_MID       0x0000
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* SONAR_MSGIDS_H */
+
+/************************/
+/*  End of File Comment */
+/************************/
