993c8656bf246e46808d9565bd3c1fb71404f52f
Mathew Benson <mbenson@windhoverlabs.com>
2019-03-06 22:10:51 -0600

    Squashed 'apps/sens/' content from commit f012440
    git-subtree-dir: apps/sens
    git-subtree-split: f01244066caf057bf372c3d8fff190114503eafb

---
+++ cdr/index.js
@@ -0,0 +1,38 @@
+'use strict';
+
+var path = require( 'path' );
+
+const CdrPlugin = require( path.join( global.CDR_INSTALL_DIR, '/commander/classes/CdrPlugin' ) ).CdrPlugin;
+
+module.exports = class CfeCdrPlugin extends CdrPlugin {
+  constructor( urlBase ) {
+    super( 'sens', path.join( __dirname, 'web', urlBase ) );
+  }
+
+  getContent() {
+    var result = {
+      shortDescription: 'Sensor Combined',
+      longDescription: 'Sensor Combined Message.',
+      nodes: {
+        main: {
+          type: CdrPlugin.ContentType.LAYOUT,
+          shortDescription: 'Main',
+          longDescription: 'Main SENS.',
+          filePath: '/main.lyt'
+        },
+        cdh: {
+          type: CdrPlugin.ContentType.PANEL,
+          shortDescription: 'Command and Data Handling',
+          longDescription: 'Command counters.',
+          filePath: '/cdh.pug'
+        }
+      }
+    };
+
+    return result;
+  }
+};
+
+//var plugin = new CfeCdrPlugin();
+
+//let CfeCdrPlugin = new class extends CdrPlugin {
