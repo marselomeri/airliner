35cb091dbfaaf9e1f30b6ac0d5a694e6da892caf
Mathew Benson <mbenson@windhoverlabs.com>
2019-08-13 23:07:09 -0500

    Deleted the training mfaa and mfab applications.

--- apps/mfaa/fsw/src/mfaa_symbols.c
+++
@@ -1,7 +0,0 @@
-#include "mfaa_msg.h"
-
-MFAA_NoArgCmd_t            MFAA_Noop;
-MFAA_HkTlm_t               MFAA_HkTlm;
-MFAA_SetGainCmd_t          MFAA_SetGain;
-MFAA_SetOffsetCmd_t        MFAA_SetOffset;
-MFAA_NoArgCmd_t            MFAA_SendConfig;
