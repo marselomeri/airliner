4323119fc4830c57c7b9ad9c4ba08bee4770cee6
cnalley <cnalley@windhoverlabs.com>
2019-08-30 20:57:41 +0000

    Implements WINDEV-779 in-progress debug of gazebo in docker.

--- tools/docker/run.sh
+++ tools/docker/run.sh
@@ -1,8 +1,12 @@
-#!/bin/bash
 sudo docker run --rm -ti \
                 -v "${HOME}/.Xauthority:/root/.Xauthority:rw" \
                 --env="DISPLAY" \
                 --hostname airliner \
+                --user airliner \
+                --workdir /home/airliner/ \
                 --cap-add=SYS_ADMIN \
+                --security-opt apparmor:unconfined \
+                --net=bridge \
+                -p 3000:3000/udp \
                 airliner:0.0.1 \
                 bash
