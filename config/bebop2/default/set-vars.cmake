df00bc534289b131eb07bf0d5436d6b106007ddf
Mathew Benson <mbenson@windhoverlabs.com>
2020-07-14 17:44:51 -0500

    Fixed host build in the ocpoc configuration.

--- config/bebop2/default/set-vars.cmake
+++ config/bebop2/default/set-vars.cmake
@@ -31,15 +31,5 @@
 #
 #############################################################################
  
-set(TARGET_PSP ${PSP_TARGET_DIR}/bebop2)
-set(TARGET_OSAL ${OSAL_TARGET_DIR}/posix-fast)
-set(HOST_PSP ${PSP_TARGET_DIR}/pc-linux-wh)
-set(HOST_OSAL ${OSAL_TARGET_DIR}/posix-fast)
-
-
-
-
-
-
-
-
+set(PSP ${PSP_TARGET_DIR}/bebop2)
+set(OSAL ${OSAL_TARGET_DIR}/posix-fast)
