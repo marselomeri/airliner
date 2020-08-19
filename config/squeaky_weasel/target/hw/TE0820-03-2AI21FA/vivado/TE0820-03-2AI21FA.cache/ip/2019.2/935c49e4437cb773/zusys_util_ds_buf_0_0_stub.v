e8b0706f5d69267ab5678f1ef0b8671ac044bd94
Mathew Benson <mbenson@windhoverlabs.com>
2020-08-19 00:21:29 -0500

    Base build is building.

---
+++ config/squeaky_weasel/target/hw/TE0820-03-2AI21FA/vivado/TE0820-03-2AI21FA.cache/ip/2019.2/935c49e4437cb773/zusys_util_ds_buf_0_0_stub.v
@@ -0,0 +1,22 @@
+// Copyright 1986-2019 Xilinx, Inc. All Rights Reserved.
+// --------------------------------------------------------------------------------
+// Tool Version: Vivado v.2019.2 (lin64) Build 2708876 Wed Nov  6 21:39:14 MST 2019
+// Date        : Wed Aug 19 00:02:39 2020
+// Host        : vagrant running 64-bit Ubuntu 16.04.6 LTS
+// Command     : write_verilog -force -mode synth_stub -rename_top decalper_eb_ot_sdeen_pot_pi_dehcac_xnilix -prefix
+//               decalper_eb_ot_sdeen_pot_pi_dehcac_xnilix_ zusys_util_ds_buf_0_0_stub.v
+// Design      : zusys_util_ds_buf_0_0
+// Purpose     : Stub declaration of top-level module interface
+// Device      : xczu2cg-sfvc784-1-i
+// --------------------------------------------------------------------------------
+
+// This empty module with port declaration file causes synthesis tools to infer a black box for IP.
+// The synthesis directives are for Synopsys Synplify support to prevent IO buffer insertion.
+// Please paste the declaration into a Verilog source file or add the file as an additional source.
+(* x_core_info = "util_ds_buf,Vivado 2019.2" *)
+module decalper_eb_ot_sdeen_pot_pi_dehcac_xnilix(IBUF_DS_P, IBUF_DS_N, IBUF_OUT)
+/* synthesis syn_black_box black_box_pad_pin="IBUF_DS_P[0:0],IBUF_DS_N[0:0],IBUF_OUT[0:0]" */;
+  input [0:0]IBUF_DS_P;
+  input [0:0]IBUF_DS_N;
+  output [0:0]IBUF_OUT;
+endmodule
---
+++ config/squeaky_weasel/target/hw/TE0820-03-2AI21FA/vivado/TE0820-03-2AI21FA.srcs/sources_1/bd/zusys/ip/zusys_labtools_fmeter_0_0/zusys_labtools_fmeter_0_0_stub.v
@@ -0,0 +1,23 @@
+// Copyright 1986-2019 Xilinx, Inc. All Rights Reserved.
+// --------------------------------------------------------------------------------
+// Tool Version: Vivado v.2019.2 (lin64) Build 2708876 Wed Nov  6 21:39:14 MST 2019
+// Date        : Wed Aug 19 00:01:31 2020
+// Host        : vagrant running 64-bit Ubuntu 16.04.6 LTS
+// Command     : write_verilog -force -mode synth_stub
+//               /home/vagrant/git/airliner/config/squeaky_weasel/target/hw/TE0820-03-2AI21FA/vivado/TE0820-03-2AI21FA.srcs/sources_1/bd/zusys/ip/zusys_labtools_fmeter_0_0/zusys_labtools_fmeter_0_0_stub.v
+// Design      : zusys_labtools_fmeter_0_0
+// Purpose     : Stub declaration of top-level module interface
+// Device      : xczu2cg-sfvc784-1-i
+// --------------------------------------------------------------------------------
+
+// This empty module with port declaration file causes synthesis tools to infer a black box for IP.
+// The synthesis directives are for Synopsys Synplify support to prevent IO buffer insertion.
+// Please paste the declaration into a Verilog source file or add the file as an additional source.
+(* x_core_info = "labtools_fmeter,Vivado 2019.2" *)
+module zusys_labtools_fmeter_0_0(refclk, fin, F0, update)
+/* synthesis syn_black_box black_box_pad_pin="refclk,fin[0:0],F0[31:0],update" */;
+  input refclk;
+  input [0:0]fin;
+  output [31:0]F0;
+  output update;
+endmodule
